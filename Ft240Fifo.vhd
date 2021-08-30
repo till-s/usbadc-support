eb15f92a8bfce8d2cfaa74a89b30f117bb9fe096
till straumann <till.straumann@alumni.tu-berlin.de>
2021-08-30 16:24:32 +0200

    Ft240Fifo.vhd: 'working' version (minimal testing; echoes characters)...

--- Ft240Fifo.vhd
+++ Ft240Fifo.vhd
@@ -61,16 +61,34 @@ architecture rtl of Ft240Fifo is
 
    type   MuxState        is ( IDLE, READ, WRITE );
 
-   signal rdataBuf        : std_logic_vector(7 downto 0);
-   signal wdataBuf        : std_logic_vector(7 downto 0);
-   signal rdataVld        : std_logic := '0';
-   signal wdataRdy        : std_logic := '1';
+   type   RegType         is  record
+      rdataBuf        : std_logic_vector(7 downto 0);
+      wdataBuf        : std_logic_vector(7 downto 0);
+      rdataVld        : std_logic;
+      wdataRdy        : std_logic;
 
-   signal dly             : unsigned(RD_CNT_LD_C - 1 downto 0) := HSK_STBL_CNT_C;
-   signal pulse           : std_logic := '1';
-   signal handshakeStable : std_logic := '0';
+      dly             : unsigned(RD_CNT_LD_C - 1 downto 0);
+      pulse           : std_logic;
+      handshakeStable : std_logic;
 
-   signal pulseMux        : MuxState  := IDLE;
+      pulseMux        : MuxState;
+   end record RegType;
+
+   constant REG_INIT_C : RegType := (
+      rdataBuf        => (others => 'X'),
+      wdataBuf        => (others => 'X'),
+      rdataVld        => '0',
+      wdataRdy        => '1',
+
+      dly             => HSK_STBL_CNT_C,
+      pulse           => '1',
+      handshakeStable => '0',
+
+      pulseMux        => IDLE
+   );
+
+   signal r               : RegType := REG_INIT_C;
+   signal rin             : RegType;
 
    -- direction control of the data I/O buffer
    signal iot             : std_logic := '1';
@@ -78,7 +96,6 @@ architecture rtl of Ft240Fifo is
    signal rxeSyn          : std_logic;
    signal txfSyn          : std_logic;
 
-   signal handshake       : std_logic;
    signal rdb             : std_logic;
    signal wr              : std_logic;
 
@@ -122,102 +139,105 @@ begin
 
    -- slow but simple...
 
+   P_COMB : process ( r, rrdy, wvld, wdat, fifoRDT, rxeSyn, txfSyn ) is
+      variable v : RegType;
+   begin
+      v := r;
+
+      -- deliver rdata
+      if ( (r.rdataVld and rrdy) = '1' ) then
+         v.rdataVld := '0';
+      end if;
+
+      -- buffer wdata 
+      if ( (r.wdataRdy and wvld) = '1' ) then
+         v.wdataRdy := '0';
+         v.wdataBuf := wdat;
+      end if;
+
+      -- handle timeouts
+      if ( r.dly = 0 ) then
+         if ( r.pulse = '0' ) then
+            v.pulse           := '1';
+            if ( r.pulseMux = READ ) then
+               v.rdataBuf     := fifoRDT;
+               v.rdataVld     := '1';
+            else
+               v.wdataRdy     := '1';
+            end if;
+            v.handshakeStable := '0';
+            v.dly             := HSK_STBL_CNT_C;
+         else
+            v.handshakeStable := '1';
+            v.pulseMux        := IDLE;
+         end if;
+      else
+         v.dly  := r.dly - 1;
+      end if;
+
+      -- issue a cycle to the FIFO
+
+      if (     r.handshakeStable = '1'
+           and r.pulse           = '1'
+           and r.pulseMux        = IDLE ) then
+
+         if    ( (r.wdataRdy = '0') and (txfSyn = '0') ) then
+            -- write over read
+            v.pulseMux := WRITE;
+         elsif ( (r.rdataVld = '0') and (rxeSyn = '0') ) then
+            v.pulseMux := READ;
+         end if;
+
+         if ( v.pulseMux /= IDLE ) then
+            v.pulse := '0';
+            v.dly   := to_unsigned(RD_PULSE_WIDTH_C - 1, r.dly'length);
+         end if;
+      end if;
+
+      rin <= v;
+   end process P_COMB;
+
    P_SEQ : process ( clk ) is
    begin
       if ( rising_edge( clk ) ) then
          if ( rst = '1') then
-            rdataVld        <= '0';
-            wdataRdy        <= '1';
-            pulse           <= '1';
-            dly             <= HSK_STBL_CNT_C;
-            handshakeStable <= '0';
+            r <= REG_INIT_C;
          else
-            if ( dly /= 0 ) then
-               dly  <= dly - 1;
-            end if;
-
-            -- deliver rdata
-            if ( (rdataVld and rrdy) = '1' ) then
-               rdataVld <= '0';
-            end if;
-
-            -- buffer wdata 
-            if ( (wdataRdy and wvld) = '1' ) then
-               wdataRdy <= '0';
-               wdataBuf <= wdat;
-            end if;
-
-            -- issue a cycle to the FIFO
-
-            if ( rdataVld = '0' or wdataRdy = '0' ) then
-               if ( pulseMux = IDLE ) then
-                  if ( rdataVld = '0' ) then
-                     -- prioritize read over write
-                     pulseMux <= READ;
-                  else
-                     pulseMux <= WRITE;
-                  end if;
-               end if;
-               if ( (pulse and handshakeStable) = '1' ) then
-                  if ( rxeSyn = '0' ) then
-                     pulse <= '0';
-                     dly   <= to_unsigned(RD_PULSE_WIDTH_C - 1, dly'length);
-                  end if;
-               else
-                  if ( dly = 0 ) then
-                     if ( pulse = '0' ) then
-                        pulse           <= '1';
-                        if ( pulseMux = READ ) then
-                           rdataBuf        <= fifoRDT;
-                           rdataVld        <= '1';
-                        else
-                           wdataRdy        <= '1';
-                        end if;
-                        handshakeStable <= '0';
-                        dly             <= HSK_STBL_CNT_C;
-                     else
-                        handshakeStable <= '1';
-                        pulseMux        <= IDLE;
-                     end if;
-                  end if;
-               end if;
-            end if;
+            r <= rin;
          end if;
       end if;
    end process P_SEQ;
 
-   P_MUX : process( pulseMux, rxeSyn, txfSyn, pulse ) is
+   P_MUX : process( r ) is
    begin
-      if ( pulseMux = WRITE ) then
-         wr        <= not pulse;
-         handshake <= txfSyn;
+      if ( r.pulseMux = WRITE ) then
+         wr        <= not r.pulse;
          rdb       <= '1';
       else
-         rdb       <= pulse;
-         handshake <= rxeSyn;
+         rdb       <= r.pulse;
          wr        <= '0';
       end if;
    end process P_MUX;
 
-   rdat    <= rdataBuf;
-   rvld    <= rdataVld;
+   rdat    <= r.rdataBuf;
+   rvld    <= r.rdataVld;
    fifoRDb <= rdb;
 
-   wrdy    <= wdataRdy;
+   wrdy    <= r.wdataRdy;
 
    fifoWR  <= wr;
-   fifoWDT <= wdataBuf;
+   fifoWDT <= r.wdataBuf;
    fifoIOT <= not wr;
 
    ila_trg0(0) <= fifoRXE;
    ila_trg0(1) <= rdb;
-   ila_trg0(2) <= rdataVld;
+   ila_trg0(2) <= r.rdataVld;
    ila_trg0(3) <= rrdy;
 
    ila_trg0(4) <= fifoTXF;
    ila_trg0(5) <= wr;
    ila_trg0(6) <= wvld;
-   ila_trg0(7) <= wdataRdy;
+   ila_trg0(7) <= r.wdataRdy;
 
    U_ICON : component chipscope_icon
       port map (
