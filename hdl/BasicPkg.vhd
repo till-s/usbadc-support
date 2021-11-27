abb3a504c076eedcde9cb0807722978d4216cae1
till straumann <till.straumann@alumni.tu-berlin.de>
2021-11-27 13:35:09 +0100

    BasicPkg.vhd: added more utilities

--- hdl/BasicPkg.vhd
+++ hdl/BasicPkg.vhd
@@ -4,16 +4,29 @@ use     ieee.math_real.all;
 
 package BasicPkg is
 
-   function numBits(constant x : integer) return integer;
+   function numBits(constant x : in integer) return integer;
+
+   function toSl(constant x : in boolean) return std_logic;
+
+   function ite(constant x : in boolean; constant a,b: in std_logic) return std_logic;
 
 end package BasicPkg;
 
 package body BasicPkg is
 
-   function numBits(constant x : integer) return integer is
+   function numBits(constant x : in integer) return integer is
    begin
       if ( x = 0 ) then return 1; end if;
       return integer( floor( log2( real( x ) ) ) ) + 1;
    end function numBits;
 
+   function toSl(constant x : in boolean) return std_logic is
+   begin
+      if ( x ) then return '1'; else return '0'; end if;
+   end function toSl;
+
+   function ite(constant x : in boolean; constant a,b: in std_logic) return std_logic is
+   begin
+      if ( x ) then return a; else return b; end if;
+   end function ite;
 end package body BasicPkg;
