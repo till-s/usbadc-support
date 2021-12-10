fc41a71229d7fa8ed7c8c9e3e742f2c39b794ced
till straumann <till.straumann@alumni.tu-berlin.de>
2021-12-10 17:51:23 +0100

    dac.sh: FIX CH1 must use external ref when CH0 uses bandgap
    Follow data sheet instructions (CH0 bandgap buffer goes to
    VREF and Ch1 picks it up when using external)

--- sw/dac.sh
+++ sw/dac.sh
@@ -5,4 +5,15 @@ fi
 # reset
 ${BB} -D -- -1
 # select internal bandgap (leave at gain 1)
-${BB} -D 8 0x5
+# according to datasheet, if we use the internal
+# bandgap then
+#  - all channels must use it
+#  - select on ch0
+#  - other channels must be in external, buffered mode
+${BB} -D 8   0x000D
+#
+# Buffer op-amp:
+#   Vo 10/15 + Vref 5/15 = Vdac 10/15
+# -> Vo = Vdac - Vref/2
+# I.e., gain 1 is to be used
+#${BB} -D 0xa 0x0000
