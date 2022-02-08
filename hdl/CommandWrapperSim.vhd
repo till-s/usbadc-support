be2da8b0fb8642963bc12ee69b1f8f9079d5fb93
till straumann <till.straumann@alumni.tu-berlin.de>
2022-02-08 20:17:28 +0100

    CommandWrapperSim.vhd: added SpiReg instance (for testing)

--- hdl/CommandWrapperSim.vhd
+++ hdl/CommandWrapperSim.vhd
@@ -13,6 +13,13 @@ architecture sim of CommandWrapperSim is
    constant MEM_DEPTH_C : natural := 1024;
    constant ADC_FIRST_C : unsigned(7 downto 0)   := x"A0";
 
+   constant BB_SPI_CSb_C  : natural := 0;
+   constant BB_SPI_SCK_C  : natural := 1;
+   constant BB_SPI_MSO_C  : natural := 2;
+   constant BB_SPI_MSI_C  : natural := 3;
+   constant BB_SPI_T_C    : natural := 6;
+
+
    signal clk     : std_logic := '0';
    signal rst     : std_logic_vector(4 downto 0) := (others => '1');
 
@@ -35,6 +42,22 @@ architecture sim of CommandWrapperSim is
    signal dorA    : std_logic                    := '0';
    signal dorB    : std_logic                    := '0';
    signal dorDDR  : std_logic                    := '0';
+
+   signal spirReg : std_logic_vector(7 downto 0) := (others => '0');
+   signal spirWen : std_logic;
+   signal spirRen : std_logic;
+   signal spirInp : std_logic_vector(7 downto 0);
+   signal spirCsb : std_logic := '1';
+   signal spirDO  : std_logic;
+   signal spirDI  : std_logic;
+
+   signal sclk    : std_logic;
+   signal mosi    : std_logic;
+   signal scsb    : std_logic;
+   signal shiz    : std_logic;
+   signal miso    : std_logic;
+
+   signal subCmdBB: SubCommandBBType;
 
 begin
 
@@ -80,6 +103,7 @@ begin
 
          bbo          => bbo,
          bbi          => bbi,
+         subCmdBB     => subCmdBB,
 
          adcClk       => clk,
          adcRst       => rst(rst'left),
@@ -135,6 +159,47 @@ begin
    begin
       report "BBO: " & std_logic'image(bbo(1)) & std_logic'image(bbo(0));
    end process P_BBMON;
-   
+
+   U_SPIREG : entity work.SpiReg
+      port map (
+         clk        => clk,
+         rst        => rst(rst'left),
+
+         sclk       => sclk,
+         scsb       => spirCsb,
+         mosi       => spirDI,
+         miso       => spirDO,
+
+         data_inp   => spirReg,
+         rs         => spirRen,
+         data_out   => spirInp,
+         ws         => spirWen
+      );
+
+   sclk              <= bbo(BB_SPI_SCK_C);
+   mosi              <= bbo(BB_SPI_MSO_C);
+   scsb              <= bbo(BB_SPI_CSb_C);
+   shiz              <= bbo(BB_SPI_T_C  );
+
+   bbi(BB_SPI_SCK_C) <= bbo(BB_SPI_SCK_C);
+   bbi(BB_SPI_CSb_C) <= bbo(BB_SPI_CSb_C);
+   bbi(BB_SPI_MSO_C) <= bbo(BB_SPI_MSO_C);
+   bbi(BB_SPI_T_C  ) <= '0';
+   bbi(BB_SPI_MSI_C) <= miso;
+
+   spirDI   <= mosi   when shiz = '0' else spirDO;
+   spirCsb  <= scsb   when subCmdBB = CMD_BB_SPI_FEG_C else '1';
+   miso     <= spirDO when subCmdBB = CMD_BB_SPI_FEG_C else 'X';
+
+   P_SPIR : process ( clk ) is
+   begin
+      if ( rising_edge(clk) ) then
+         if ( rst(rst'left) = '1' ) then
+            spirReg <= (others => '0');
+         elsif ( spirWen = '1' ) then
+            spirReg <= spirInp;
+         end if;
+      end if;
+   end process P_SPIR;
 
 end architecture sim;
