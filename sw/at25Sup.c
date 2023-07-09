3f6222eeaea75d12e8e4c26a991254e0a49aab96
till straumann <till.straumann@alumni.tu-berlin.de>
2023-07-09 17:56:27 +0200

    at25Sup.c: support new SPI controller (WIP)

--- sw/at25Sup.c
+++ sw/at25Sup.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "cmdXfer.h"
 #include "fwComm.h"
@@ -23,7 +24,9 @@
 #define AT25_ST_WEL        0x02
 #define AT25_ST_EPE        0x20
 
-static int verify(FWInfo *fw, unsigned addr, const uint8_t *cmp, size_t len);
+static int verify(FWInfo *fw, unsigned addr, const uint8_t *cmp, size_t len, int addnl);
+
+static int do_xfer(FWInfo *fw, const uint8_t *hdr, unsigned hlen, const uint8_t *tbuf, uint8_t *rbuf, unsigned buflen);
 
 int
 at25_id(FWInfo *fw)
@@ -37,7 +40,7 @@ int     i;
 	buf[3] = 0x00;
 	buf[4] = 0x00;
 
-	if ( bb_spi_xfer( fw, SPI_FLASH, buf, buf + 0x10, 0, 5 ) < 0 ) {
+	if ( do_xfer( fw, 0, 0, buf, buf + 0x10, 5 ) < 0 ) {
 		return -1;
 	}
 
@@ -53,7 +56,7 @@ at25_spi_read(FWInfo *fw, unsigned addr, uint8_t *rbuf, size_t len)
 {
 uint8_t  hdr[10];
 unsigned hlen = 0;
-int      rval = -1;
+int      st;
 
 	hdr[hlen++] = AT25_OP_FAST_READ;
 	hdr[hlen++] = (addr >> 16) & 0xff;
@@ -61,23 +64,12 @@ int      rval = -1;
 	hdr[hlen++] = (addr >>  0) & 0xff;
 	hdr[hlen++] = 0x00; /* dummy     */
 
-	if ( bb_spi_cs( fw, SPI_FLASH, 0 ) < 0 ) {
+	if ( (st = do_xfer( fw, hdr, hlen, rbuf, rbuf, len )) != hlen + len ) {
+		fprintf(stderr,"at25_spi_read -- receiving data failed or incomplete st %d, hlen %d, len %d\n", st, hlen, (unsigned)len);
 		return -1;
 	}
 
-	if ( bb_spi_xfer_nocs( fw, SPI_FLASH, hdr, 0, 0, hlen ) != hlen ) {
-		fprintf(stderr,"at25_spi_read -- sending header failed\n");
-		goto bail;
-	}
-
-	if ( ( rval = bb_spi_xfer_nocs( fw, SPI_FLASH, 0, rbuf, 0, len ) ) != len ) {
-		fprintf(stderr,"at25_spi_read -- receiving data failed or incomplete\n");
-		goto bail;
-	} 
-
-bail:
-	bb_spi_cs( fw, SPI_FLASH, 1 );
-	return rval;
+	return len;
 }
 
 int
@@ -85,8 +77,8 @@ at25_status(FWInfo *fw)
 {
 uint8_t buf[2];
 	buf[0] = AT25_OP_STATUS;
-	if ( bb_spi_xfer( fw, SPI_FLASH, buf, buf, 0, sizeof(buf) ) < 0 ) {
-		fprintf(stderr, "at25_status: bb_spi_xfer failed\n");
+	if ( do_xfer( fw, 0, 0, buf, buf, sizeof(buf) ) < 0 ) {
+		fprintf(stderr, "at25_status: spi_xfer failed\n");
 		return -1;
 	}
 	return buf[1];
@@ -103,7 +95,7 @@ int     len = 0;
 		buf[len++] = arg;
 	}
 
-	if ( bb_spi_xfer( fw, SPI_FLASH, buf, 0, 0, len ) < 0 ) {
+	if ( do_xfer( fw, 0, 0, buf, buf, len ) < 0 ) {
 		fprintf(stderr, "at25_cmd_2(0x%02x) transfer failed\n", cmd);
 		return -1;
 	}
@@ -129,18 +121,30 @@ at25_write_dis(FWInfo *fw)
 }
 
 int
+at25_status01_write(FWInfo *fw, uint8_t val)
+{
+int st;
+	if ( (st = at25_cmd_2    ( fw, AT25_OP_STATUS_WR, val )) ) {
+		return st;
+	}
+	/* must wait until not busy anymore */
+	return at25_status_poll( fw );
+}
+
+int
 at25_global_unlock(FWInfo *fw)
 {
 	/* must se write-enable again; global_unlock clears that bit */
-	return   at25_write_ena( fw )
-          || at25_cmd_2    ( fw, AT25_OP_STATUS_WR, 0x00 )
-          || at25_write_ena( fw );
+	return    at25_write_ena( fw )
+          || (at25_status01_write( fw, 0x00 ) < 0)
+          ||  at25_write_ena( fw );
 }
 
 int
 at25_global_lock(FWInfo *fw)
 {
-	return at25_write_ena( fw ) || at25_cmd_2( fw, AT25_OP_STATUS_WR, 0x3c );
+	return    at25_write_ena( fw )
+          || (at25_status01_write( fw, 0x3c ) < 0);
 }
 
 int
@@ -148,7 +152,7 @@ at25_status_poll(FWInfo *fw)
 {
 int st;
 		/* poll status */
-	do { 
+	do {
 		if ( (st = at25_status( fw )) < 0 ) {
 			fprintf(stderr, "at25_status_poll() - failed to read status\n");
 			break;
@@ -161,7 +165,7 @@ int st;
 int
 at25_block_erase(FWInfo *fw, unsigned addr, size_t sz)
 {
-uint8_t op = AT25_OP_ERASE_ALL; 
+uint8_t op = AT25_OP_ERASE_ALL;
 
 uint8_t buf[4];
 int     l, st;
@@ -184,8 +188,8 @@ int     l, st;
 		buf[l++] = (addr >>  0) & 0xff;
 	}
 
-	if ( bb_spi_xfer( fw, SPI_FLASH, buf, 0, 0, l ) < 0 ) {
-		fprintf(stderr, "at25_block_erase() -- sending command failed\n"); 
+	if ( do_xfer( fw, 0, 0, buf, buf, l ) < 0 ) {
+		fprintf(stderr, "at25_block_erase() -- sending command failed\n");
 		return -1;
 	}
 
@@ -203,7 +207,7 @@ int     l, st;
 
 }
 
-static int verify(FWInfo *fw, unsigned addr, const uint8_t *cmp, size_t len)
+static int verify(FWInfo *fw, unsigned addr, const uint8_t *cmp, size_t len, int addnl)
 {
 uint8_t   buf[2048];
 int       mismatch = 0;
@@ -233,7 +237,9 @@ int       got,i;
 			}
 			printf("%c", cmp ? 'v' : 'z'); fflush(stdout);
 		}
-		printf("\n");
+		if ( addnl ) {
+			printf("\n");
+		}
 		return mismatch ? -1 : 0;
 }
 
@@ -247,9 +253,10 @@ int            i;
 int            rval  = -1;
 int            st;
 const uint8_t *src;
+uint8_t        junk[AT25_PAGE];
 
 	if ( (check & AT25_CHECK_ERASED) ) {
-		if ( verify( fw, addr, 0, len ) )
+		if ( verify( fw, addr, 0, len, 1 ) )
 			return -1;
 	}
 
@@ -265,11 +272,6 @@ const uint8_t *src;
 
 		while ( wrk > 0 ) {
 
-			if ( bb_spi_cs( fw, SPI_FLASH, 0 ) ) {
-				fprintf(stderr, "at25_prog() - failed to assert CSb\n");
-				goto bail;
-			}
-
 			/* page-align the end of the write */
 
 			x = ( (wrkAddr + AT25_PAGE) & ~ (AT25_PAGE - 1) ) - wrkAddr;
@@ -283,21 +285,22 @@ const uint8_t *src;
 			buf[i++] = (wrkAddr >> 16) & 0xff;
 			buf[i++] = (wrkAddr >>  8) & 0xff;
 			buf[i++] = (wrkAddr >>  0) & 0xff;
-			if ( bb_spi_xfer_nocs( fw, SPI_FLASH, buf, 0, 0, i ) < 0 ) {
-				fprintf(stderr, "at25_prog() - failed to transmit address\n");
-				goto bail;
-			}
-			if ( bb_spi_xfer_nocs( fw, SPI_FLASH, src, 0, 0, x ) < 0 ) {
-				fprintf(stderr, "at25_prog() - failed to transmit data\n");
+
+			if ( do_xfer( fw, buf, i, src, junk, x ) < 0 ) {
+				fprintf(stderr, "at25_prog() - failed to transmit\n");
 				goto bail;
 			}
 
-			/* this triggers the write */
-			if ( bb_spi_cs( fw, SPI_FLASH, 1 ) ) {
-				fprintf(stderr, "at25_prog() - failed to de-assert CSb\n");
-				goto bail;
+			/* should not be necessary but ensure CS is not
+			 * reasserted too quickly (100ns) is specified (AT25SL641) but
+			 * it is unclear if that applies to status queries, too.
+			 */
+			{
+				struct timespec t;
+				t.tv_sec  = 0;
+				t.tv_nsec = 100000;
+				nanosleep( &t, 0 );
 			}
-
 
 			if ( (st = at25_status_poll( fw )) < 0 ) {
 				fprintf(stderr, "at25_prog() - failed to poll status\n");
@@ -307,6 +310,12 @@ const uint8_t *src;
 			if ( (st & AT25_ST_EPE) ) {
 				fprintf(stderr, "at25_status_poll() -- programming error (status 0x%02x, writing page 0x%x) -- aborting\n", st, wrkAddr);
 				goto bail;
+			}
+
+			if ( 1 ) {
+				if ( verify( fw, wrkAddr, src, x, 0 ) ) {
+					return -1;
+				}
 			}
 
 			/* programming apparently disables writing */
@@ -323,20 +332,100 @@ const uint8_t *src;
 	}
 
 	if ( (check & AT25_CHECK_VERIFY) ) {
-		if ( verify( fw, addr, data, len ) ) {
+		if ( verify( fw, addr, data, len, 1 ) ) {
 			goto bail;
 		}
 	}
 
 	rval = len;
-	
+
 bail:
 	if ( (check & AT25_EXEC_PROG) ) {
 		at25_global_lock( fw ); /* if this succeeds it clears the write-enable bit */
 		at25_write_dis  ( fw ); /* just in case... */
 	}
 
-	bb_spi_cs( fw, SPI_NONE, 1 );     /* just in case... */
-
 	return rval;
+}
+
+static int
+do_xfer(FWInfo *fw, const uint8_t *hdr, unsigned hlen, const uint8_t *tbuf, uint8_t *rbuf, unsigned buflen)
+{
+uint8_t cmd = fw_get_cmd( FW_CMD_SPI );
+tbufvec tv[2];
+rbufvec rv[2];
+size_t  nt = 0;
+size_t  nr = 0;
+uint8_t hbuf[64];
+
+	if ( hlen > sizeof(hbuf) ) {
+		fprintf(stderr, "Internal error - header too big\n");
+		abort();
+	}
+
+	if ( hlen > 0 ) {
+		tv[nt].buf = hdr;
+		tv[nt].len = hlen;
+		nt++;
+		rv[nr].buf = hbuf;
+		rv[nr].len = hlen;
+		nr++;
+	}
+	if ( buflen > 0 ) {
+		tv[nt].buf = tbuf;
+		tv[nt].len = buflen;
+		nt++;
+		rv[nr].buf = rbuf;
+		rv[nr].len = buflen;
+		nr++;
+	}
+	return fw_xfer_vec( fw, cmd, tv, nt, rv, nr );
+
+}
+
+static int
+do_xfer_old(FWInfo *fw, const uint8_t *hdr, unsigned hlen, const uint8_t *tbuf, uint8_t *rbuf, unsigned buflen)
+{
+unsigned onelen = 0;
+int      rv     = 0;
+int      st;
+
+	if ( !  ( hlen > 0 && buflen > 0 ) ) {
+		/* only one of tlen, hlen can be != 0 */
+		onelen = hlen + buflen;
+	}
+
+	if ( 0 != onelen ) {
+		return bb_spi_xfer( fw, SPI_FLASH, (hlen > 0 ) ? hdr : tbuf, rbuf, 0, onelen );
+	} else {
+		if ( bb_spi_cs( fw, SPI_FLASH, 0 ) < 0 ) {
+			fprintf(stderr, "do_xfer: bb_spi_cs failed to set CS low\n");
+			goto bail;
+		}
+
+		if ( hlen > 0 ) {
+			st = bb_spi_xfer_nocs( fw, SPI_FLASH, hdr, 0, 0, hlen );
+			if ( st < 0 ) {
+				fprintf(stderr, "do_xfer: bb_spi_xfer failed to send header\n");
+				rv = -1;
+				goto bail;
+			}
+			rv += st;
+		}
+		if ( buflen > 0 ) {
+			st = bb_spi_xfer_nocs( fw, SPI_FLASH, tbuf, rbuf, 0, buflen );
+			if ( st < 0 ) {
+				fprintf(stderr, "do_xfer: bb_spi_xfer failed to send data\n");
+				rv = -1;
+				goto bail;
+			}
+			rv += st;
+		}
+bail:
+		if ( bb_spi_cs( fw, SPI_FLASH, 1 ) < 0 ) {
+			fprintf(stderr, "do_xfer: bb_spi_cs failed to set CS high\n");
+			rv = -1;
+		}
+		return rv;
+	}
 }
