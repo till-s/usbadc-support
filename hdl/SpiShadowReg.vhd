0ccc924778f527d17e192a828931164f0f36673a
till straumann <till.straumann@alumni.tu-berlin.de>
2023-07-17 21:47:58 +0200

    Added SpiShadowReg
    A module that caches register values for SPI write-only devices
    such as the AD8370 VGA.

---
+++ hdl/SpiShadowReg.vhd
@@ -0,0 +1,188 @@
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.numeric_std.all;
+
+use     work.BasicPkg.all;
+
+-- shadow registers for SPI write-only devices;
+-- provide read-back of current values.
+-- NOTES: - Supports SPI mode 0 only!
+--        - clk must be at least 4x as fast as sclkIb
+
+
+entity SpiShadowReg is
+   generic (
+      NUM_REGS_G         : positive range 1 to 31;
+      -- register initialization (optional); *ascending* index
+      REG_INIT_G         : Slv8Array := SLV8_ARRAY_EMPTY_C
+   );
+   port (
+      clk                : in  std_logic;
+      rst                : in  std_logic := '0';
+
+      sclkIb             : in  std_logic;
+      mosiIb             : in  std_logic;
+      scsbIb             : in  std_logic;
+      misoIb             : out std_logic;
+
+      sclkOb             : out std_logic_vector(NUM_REGS_G - 1 downto 0);
+      scsbOb             : out std_logic_vector(NUM_REGS_G - 1 downto 0)
+      -- no misoOb since the registers we shadow are supposed to be
+      -- write-only
+   );
+end entity SpiShadowReg;
+
+architecture rtl of SpiShadowReg is
+
+   type StateType is ( IDLE, DATA, DEASS_CS, DRAIN );
+
+   constant OP_READ_C  : std_logic := '1';
+   constant CLK_ACT_C  : std_logic := '1';
+
+   function REG_INIT_F return Slv8Array is
+      variable v : Slv8Array(0 to NUM_REGS_G - 1);
+   begin
+      if ( REG_INIT_G'length = 0 ) then
+         v := (others => (others => '0'));
+      else
+         v := REG_INIT_G;
+      end if;
+      return v;
+   end function;
+
+   type RegType is record
+      state       : StateType;
+      lscsb       : std_logic;
+      regs        : Slv8Array(0 to NUM_REGS_G - 1);
+      sel         : natural range 0 to NUM_REGS_G - 1;
+      op          : std_logic;
+      phas        : std_logic;
+   end record RegType;
+
+   constant REG_INIT_C : RegType := (
+      state       => IDLE,
+      lscsb       => '1',
+      regs        => REG_INIT_F,
+      sel         => 0,
+      op          => OP_READ_C,
+      phas        => '0'
+   );
+
+   signal r                : RegType := REG_INIT_C;
+   signal rin              : RegType := REG_INIT_C;
+
+   signal rs               : std_logic;
+   signal ws               : std_logic;
+   signal shftDatIb        : Slv8Type;
+   signal shftDatOb        : Slv8Type := (others => '1');
+
+begin
+
+   U_SR : entity work.SpiReg
+      generic map (
+         FRAMED_G     => true
+      )
+      port map (
+         clk          => clk,
+         rst          => rst,
+         sclk         => sclkIb,
+         scsb         => scsbIb,
+         mosi         => mosiIb,
+         miso         => misoIb,
+         data_inp     => shftDatOb,
+         data_out     => shftDatIb,
+         rs           => rs,
+         ws           => ws
+      );
+
+   P_COMB : process ( r, sclkIb, mosiIb, scsbIb, rs, ws, shftDatIb ) is
+      variable v            : RegType;
+      variable propagateSPI : boolean;
+   begin
+      v            := r;
+
+      shftDatOb    <= ( others => '1' );
+
+      v.lscsb      := scsbIb;
+      propagateSPI := false;
+      sclkOb       <= ( others => not CLK_ACT_C );
+      scsbOb       <= ( others => '1' );
+
+      case ( r.state ) is
+         when IDLE =>
+            v.phas    := not CLK_ACT_C;
+            -- ws assertion implies cs is low
+            if ( ws = '1' ) then
+               if ( unsigned( shftDatIb(4 downto 0) ) < NUM_REGS_G ) then
+                  v.op          := shftDatIb(7);
+                  v.sel         := to_integer( unsigned( shftDatIb(4 downto 0) ) );
+                  v.state       := DATA;
+                  shftDatOb     <= r.regs( v.sel );
+                  if ( v.op /= OP_READ_C ) then
+                     -- phas is used to synchronize propagation
+                     -- of the SPI signals with the SCLK
+                     v.phas        := CLK_ACT_C;
+                     scsbOb(v.sel) <= '0';
+                  end if;
+               else
+                  v.state       := DRAIN;
+               end if;
+            end if;
+
+         when DATA =>
+            shftDatOb <= r.regs( r.sel );
+            if ( r.op /= OP_READ_C ) then
+               -- maintain CS asserted while waiting for the input clock
+               -- to deassert; the clock is not propagated yet
+               scsbOb(r.sel) <= '0';
+               -- wait for clock to be deasserted until propagating SPI
+               if ( (r.phas /= CLK_ACT_C) or (sclkIb /= CLK_ACT_C) ) then
+                  propagateSPI := true;
+                  v.phas       := not CLK_ACT_C;
+               end if;
+            end if;
+            if ( ws = '1' ) then
+               v.state := DRAIN;
+               if ( r.op /= OP_READ_C ) then
+                  v.regs( r.sel ) := shftDatIb;
+                  v.phas          := CLK_ACT_C;
+                  v.state         := DEASS_CS;
+               end if;
+            end if;
+
+         when DEASS_CS =>
+            propagateSPI := true;
+            -- wait for clock to be deasserted
+            if ( sclkIb /= CLK_ACT_C ) then
+               v.state   := DRAIN;
+            end if;
+
+         when DRAIN =>
+               -- wait until CS raising edga (see below)
+      end case;
+
+      if ( (scsbIb and not r.lscsb) = '1' ) then
+         -- CSEL was just deasserted
+         v.state := IDLE;
+      end if;
+
+      if ( propagateSPI ) then
+         sclkOb(r.sel) <= sclkIb;
+         scsbOb(r.sel) <= '0';
+      end if;
+
+      rin          <= v;
+   end process P_COMB;
+
+   P_SEQ : process ( clk ) is
+   begin
+      if ( rising_edge( clk ) ) then
+         if ( rst = '1' ) then
+            r <= REG_INIT_C;
+         else
+            r <= rin;
+         end if;
+      end if;
+   end process P_SEQ;
+
+end architecture rtl;
---
+++ hdl/SpiShadowRegTb.vhd
@@ -0,0 +1,228 @@
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.numeric_std.all;
+
+entity SpiShadowRegTb is
+end entity SpiShadowRegTb;
+
+architecture sim of SpiShadowRegTb is
+   constant N_C           : natural   := 3;
+   constant NREG_C        : natural   := 2;
+
+   type  Slv8Array is array ( natural range <> ) of std_logic_vector(7 downto 0);
+   signal clk             : std_logic := '0';
+   signal sclk            : std_logic := '0';
+   signal scsb            : std_logic_vector(N_C - 1 downto 0) := (others => '1');
+
+   signal rsSrc, wsSrc    : std_logic;
+
+   signal datSrc          : std_logic_vector(7 downto 0) := (others => 'X');
+   signal datSnk          : std_logic_vector(7 downto 0) := (others => 'X');
+
+   signal mosi            : std_logic_vector(3 downto 0);
+   signal miso            : std_logic_vector(3 downto 0);
+
+   signal run             : boolean   := true;
+
+   signal datChk          : Slv8Array(NREG_C - 1 downto 0);
+   signal wsChk           : std_logic_vector(NREG_C - 1 downto 0);
+
+   signal sclkMux         : std_logic_vector(NREG_C - 1 downto 0);
+   signal scsbMux         : std_logic_vector(NREG_C - 1 downto 0);
+
+   constant DAT_CMP_C     : Slv8Array(0 to NREG_C - 1) := (
+      0 => x"FF",
+      1 => x"5A"
+   );
+
+   signal wsChecked       : natural := 0;
+   signal rbChecked       : natural := 0;
+begin
+
+   P_CLK : process is
+   begin
+      if ( not run ) then wait; else
+         wait for 20 ns;
+         clk <= not clk;
+      end if;
+   end process P_CLK;
+
+   P_SCLK : process ( clk ) is
+      variable cnt : natural := 0;
+   begin
+      if ( rising_edge(clk) ) then
+         cnt := cnt + 1;
+         if ( cnt = 4 ) then
+            sclk <= not sclk;
+            cnt := 0;
+         end if;
+      end if;
+   end process P_SCLK;
+
+   P_FEED : process (rsSrc) is
+      variable cnt : natural := 0;
+   begin
+      datSrc <= (others => 'X');
+      if ( rising_edge(rsSrc) ) then
+         case ( cnt ) is
+            when 0 => datSrc <= x"01";
+            when 1 => datSrc <= DAT_CMP_C(1);
+            when 2 => -- last RS before CS is asserted
+            when 3 => datSrc <= x"81"; cnt := cnt + 1;
+            when 4 => datSrc <= x"FF"; cnt := cnt + 1;
+            when 5 => -- last RX before CS is asserted
+            when 6 => datSrc <= x"80"; cnt := cnt + 1;
+            when 7 => datSrc <= x"FF"; cnt := cnt + 1;
+            when others =>
+               datSrc <= (others => '1');
+         end case;
+         cnt := cnt + 1;
+      end if;
+   end process P_FEED;
+
+   P_RDBK : process (wsSrc, datSnk) is
+      variable cnt : natural := 0;
+   begin
+      if ( rising_edge( wsSrc ) ) then
+         case cnt is
+            when 3 =>
+               assert datSnk = DAT_CMP_C(1) report "data readback (1) FAILED" severity failure;
+               rbChecked <= rbChecked + 1;
+
+            when 5 =>
+               assert datSnk = DAT_CMP_C(0) report "data readback (0) FAILED" severity failure;
+               rbChecked <= rbChecked + 1;
+
+
+            when others =>
+         end case;
+         cnt := cnt + 1;
+      end if;
+   end process P_RDBK;
+
+   P_DRV : process is
+      variable cnt : natural := 0;
+   begin
+      wait until falling_edge(sclk);
+      wait until rising_edge(clk);
+      scsb(0) <= '0';
+      for i in 1 to 16 loop
+         wait until rising_edge(sclk);
+      end loop;
+      wait until falling_edge(sclk);
+      wait until rising_edge(clk);
+      scsb(0) <= '1';
+      wait until rising_edge(clk);
+   end process P_DRV;
+
+   U_DRV : entity work.SpiReg
+      generic map (
+         FRAMED_G => true
+      )
+      port map (
+         clk      => clk,
+         sclk     => sclk,
+         scsb     => scsb(0),
+         mosi     => miso(0),
+         miso     => mosi(0),
+
+         data_inp => datSrc,
+         rs       => rsSrc,
+         ws       => wsSrc,
+         data_out => datSnk
+      );
+
+   U_DUT : entity work.SpiShadowReg
+      generic map (
+         NUM_REGS_G => NREG_C,
+         REG_INIT_G => (
+                         1 => x"ff",
+                         0 => x"c6"
+                       )
+      )
+      port map (
+         clk      => clk,
+         sclkIb   => sclk,
+         scsbIb   => scsb(0),
+         mosiIb   => mosi(0),
+         misoIb   => miso(0),
+
+         sclkOb   => sclkMux,
+         scsbOb   => scsbMux
+      );
+
+   U_REG_1 : entity work.SpiReg
+      generic map (
+         FRAMED_G => true
+      )
+      port map (
+         clk      => clk,
+         sclk     => sclkMux(1),
+         scsb     => scsbMux(1),
+         mosi     => mosi(0),
+         miso     => open,
+
+         data_inp => x"FF",
+         rs       => open,
+         ws       => wsChk(1),
+         data_out => datChk(1)
+      );
+
+   U_REG_0 : entity work.SpiReg
+      generic map (
+         FRAMED_G => true
+      )
+      port map (
+         clk      => clk,
+         sclk     => sclkMux(0),
+         scsb     => scsbMux(0),
+         mosi     => mosi(0),
+         miso     => open,
+
+         data_inp => x"FF",
+         rs       => open,
+         ws       => wsChk(0),
+         data_out => datChk(0)
+      );
+
+
+   P_CHK : process ( wsChk ) is
+   begin
+      for i in wsChk'range loop
+         if ( wsChk(i) = '1' ) then
+            assert datChk(i) = DAT_CMP_C(i) report "target data mismatch" severity failure;
+            wsChecked <= wsChecked + 1;
+         end if;
+      end loop;
+   end process P_CHK;
+
+
+   U_CHK : entity work.SpiChecker
+      port  map (
+         clk      => clk,
+         sclk     => sclk,
+         scsb     => scsb(0),
+         mosi     => mosi(0),
+         miso     => miso(0)
+      );
+
+   G_CHECKERS : for i in 0 to NREG_C - 1 generate
+      U_DUT_CHK : entity work.SpiChecker
+         port  map (
+            clk      => clk,
+            sclk     => sclkMux(i),
+            scsb     => scsbMux(i),
+            mosi     => mosi(0),
+            miso     => '0'
+         );
+   end generate G_CHECKERS;
+
+   P_WRAPUP : process is
+   begin
+      wait until ( wsChecked = 1 and rbChecked = 2 );
+      report "TEST PASSED";
+      run <= false;
+      wait;
+   end process P_WRAPUP;
+
+end architecture sim;
