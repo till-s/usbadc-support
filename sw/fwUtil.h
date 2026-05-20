5c1606140242e35d405550c1ea617dfac4d3edcf
till straumann <till.straumann@alumni.tu-berlin.de>
2026-05-21 00:24:29 +0200

    sw - fwUtil.h: added comment

--- sw/fwUtil.h
+++ sw/fwUtil.h
@@ -6,6 +6,7 @@
 extern "C" {
 #endif
 
+/* NOTE: '*mapp' must be set to MAP_FAILED before calling this! */
 int
 fileMap(const char *fnam,  uint8_t **mapp, off_t *sizp, off_t creatsz, int readOnly);
 
